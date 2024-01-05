5210dc143db280f7fc71a3f5a6222dfbe5d58066
eikofee <eiko.fee@outlook.com>
2024-01-05 10:22:01 +0100

    fix rule save cors issue

--- src/app/api/rules/route.tsx
+++ src/app/api/rules/route.tsx
@@ -42,5 +42,10 @@ export async function GET(request: Request) {
             content[f] = JSON.parse((await fsPromises.readFile(p.concat("/", f))).toString())
         }
     }
-    return Response.json(content)
+    return Response.json(content, {headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      }
+  })
 }

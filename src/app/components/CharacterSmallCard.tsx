85c11d23269fe7835083522ec3f7ff790ea488b0
eikofee <eiko.fee@outlook.com>
2024-01-27 09:59:15 +0100

    improve character small cards

--- src/app/components/CharacterSmallCard.tsx
+++ src/app/components/CharacterSmallCard.tsx
@@ -2,12 +2,13 @@ import { ICharacterData } from "@/server/gamedata/ICharacterData"
 
 export default function CharacterSmallCard({uid, character, useHref, useLargeFont, useBackground, borderColor}: {uid: string, character: ICharacterData, useHref: boolean, useLargeFont: boolean, useBackground: boolean, borderColor: string}) {
     let textSize = "text-sm"
-    if (character.name.length > 10) {
+    if (character.name.length > 14) {
         textSize = "text-xs"
     }
 
     let age = Date.now() - character.lastUpdated
-    let updateIndicator = <div className="h-2 w-2 rounded-full bg-green-400"/>
+    let updateIndicator = <div className="h-2 w-2 m-1 rounded-full bg-green-600"/>
+    // let updateIndicator = <div className="text-xs[8px] text-green-700">up</div>
     if (age > 900000) {
         let h = ""
         let div = 1
@@ -25,16 +26,16 @@ export default function CharacterSmallCard({uid, character, useHref, useLargeFon
             div = 7 * 24 * 3600000
         }
         let text = Math.floor(age / div).toString().concat(h)
-        updateIndicator = <div className="text-xs[8px]">{text}</div>
+        updateIndicator = <div className="text-xs[8px] px-1">{text}</div>
     }
-    let content = <div className="items-center h-full w-full flex flex-row cursor-pointer relative">
-                        <div className="h-20 max-w-20 overflow-hidden">
+    let content = <div className="items-center h-20 w-full flex flex-row cursor-pointer relative">
+                        <div className="absolute inset-y-0 -left-2 overflow-hidden">
                             <img className="h-full" src={character.commonData.assets.characterPortrait} alt={""} />
                         </div>
-                        <div className={"p-1 text-center basis-2/3 text-ellipsis bg-slate-100/60 rounded-md ".concat(useLargeFont ? "font-bold text-xl" : textSize)}>
+                        <div className={"p-1 text-center absolute top-2 left-20 -right-2 text-ellipsis bg-slate-100/70 rounded-md ".concat(useLargeFont ? "font-bold text-xl" : textSize)}>
                             {character.name}
                         </div>
-                        <div className="text-xs absolute bottom-1 -right-2 text-right text-ellipsis bg-slate-100/60 rounded-md p-px">
+                        <div className="text-xs absolute bottom-1 -right-2 text-right text-ellipsis bg-slate-100/70 rounded-md p-px">
                             {updateIndicator}
                         </div>
                     </div>

d09c4bc61f74d31bbd02c6a8728807433e36ee92
eikofee <eiko.fee@outlook.com>
2024-08-22 14:39:19 +0200

    add missing file in previous commit

---
+++ src/server/api/ApiLogicGetPlayers.ts
@@ -0,0 +1,11 @@
+import { IApiResult } from "@/app/interfaces/IApiResult";
+import { IPlayerInfo, IPlayerInfoWithoutCharacters } from "../gamedata/IPlayerInfo";
+import { getPlayerInfoList } from "../DataLoader";
+
+export async function apiLogicGetPlayers() {
+    const res : IApiResult<IPlayerInfoWithoutCharacters[]> = {success: false}
+    const data = await getPlayerInfoList()
+    res.content = data
+    res.success = true
+    return res
+}
